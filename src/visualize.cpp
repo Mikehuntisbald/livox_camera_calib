f0ff36c50712c86568fe2508efcc680730ed73cc
Li Shiyun <72964929+Mikehuntisbald@users.noreply.github.com>
2023-09-22 12:54:11 +0800

    Add files via upload

--- src/visualize.cpp
+++ src/visualize.cpp
@@ -1,177 +1,96 @@
-#include <string>
-#include <stdio.h>
+#include "ceres/ceres.h"
+#include "common.h"
+#include "lidar_camera_calib.hpp"
 #include <fstream>
+#include <iomanip>
 #include <iostream>
+#include <opencv2/core/eigen.hpp>
+#include <string>
+#include <pcl/visualization/cloud_viewer.h>
 
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/kdtree/kdtree_flann.h>
-#include <pcl/filters/passthrough.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/filters/statistical_outlier_removal.h>
+int main(int argc, char **argv) {
 
-#include <sensor_msgs/PointCloud2.h>
-#include <geometry_msgs/PoseArray.h>
-#include <tf/transform_broadcaster.h>
-#include <visualization_msgs/Marker.h>
-#include <visualization_msgs/MarkerArray.h>
+    ros::init(argc, argv, "lidarCamCalib");
 
-#include <Eigen/Dense>
-#include <Eigen/StdVector>
-
-#include "ros/ros.h"
-#include <math.h>
-#include <rosbag/bag.h>
-#include <ceres/ceres.h>
-
-#include "ba.hpp"
-#include "tools.hpp"
-#include "mypcl.hpp"
-
-using namespace std;
-using namespace Eigen;
-
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "visualize");
-  ros::NodeHandle nh("~");
-
-  ros::Publisher pub_map = nh.advertise<sensor_msgs::PointCloud2>("/cloud_map", 100);
-  ros::Publisher pub_debug = nh.advertise<sensor_msgs::PointCloud2>("/cloud_debug", 100);
-  ros::Publisher pub_pose = nh.advertise<geometry_msgs::PoseArray>("/poseArrayTopic", 10);
-  ros::Publisher pub_trajectory = nh.advertise<visualization_msgs::Marker>("/trajectory_marker", 100);
-  ros::Publisher pub_pose_number = nh.advertise<visualization_msgs::MarkerArray>("/pose_number", 100);
-
-  string file_path;
-  double downsample_size, marker_size;
-  int pcd_name_fill_num;
-
-  nh.getParam("file_path", file_path);
-  nh.getParam("downsample_size", downsample_size);
-  nh.getParam("pcd_name_fill_num", pcd_name_fill_num);
-  nh.getParam("marker_size", marker_size);
-
-  sensor_msgs::PointCloud2 debugMsg, cloudMsg, outMsg;
-  vector<mypcl::pose> pose_vec;
-
-  pose_vec = mypcl::read_pose(file_path + "pose.json");
-  size_t pose_size = pose_vec.size();
-  cout<<"pose size "<<pose_size<<endl;
-
-  pcl::PointCloud<PointType>::Ptr pc_surf(new pcl::PointCloud<PointType>);
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr color_full(new pcl::PointCloud<pcl::PointXYZRGB>);
-
-  ros::Time cur_t;
-  geometry_msgs::PoseArray parray;
-  parray.header.frame_id = "camera_init";
-  parray.header.stamp = cur_t;
-  visualization_msgs::MarkerArray markerArray;
-
-  cout<<"push enter to view"<<endl;
-  getchar();
-  for(size_t i = 0; i < pose_size; i++)
-  {
-    mypcl::loadPCD(file_path + "pcd/", pcd_name_fill_num, pc_surf, i);
-
-    pcl::PointCloud<PointType>::Ptr pc_filtered(new pcl::PointCloud<PointType>);
-    pc_filtered->resize(pc_surf->points.size());
-    int cnt = 0;
-    for(size_t j = 0; j < pc_surf->points.size(); j++)
-    {
-      pc_filtered->points[cnt] = pc_surf->points[j];
-      cnt++;
+    ros::NodeHandle nh;
+    vector<double> camera_matrix;
+    vector<double> dist_coeffs;
+    vector<double> extrinsic;
+    std::string pcd_dir, jpg_dir;
+    nh.param<vector<double>>("camera/camera_matrix", camera_matrix,
+                               vector<double>());
+    nh.param<vector<double>>("camera/dist_coeffs", dist_coeffs, vector<double>());
+    nh.param<vector<double>>("camera/extrinsic", extrinsic, vector<double>());
+    nh.param<std::string>("camera/pcd_dir", pcd_dir, std::string);
+    nh.param<std::string>("camera/jpg_dir", jpg_dir, std::string);
+    // Load point cloud from PCD file
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    if (pcl::io::loadPCDFile<pcl::PointXYZ>("/media/sam/Extreme\ SSD/0.pcd", *cloud) == -1) {
+        PCL_ERROR("Couldn't read PCD file\n");
+        return -1;
     }
-    pc_filtered->resize(cnt);
+
+    // Load image from JPG file
+    cv::Mat image = cv::imread("/home/sam/.ros/0.jpg");
+    if (image.empty()) {
+        std::cerr << "Failed to load image." << std::endl;
+        return -1;
+    }
     
-    mypcl::transform_pointcloud(*pc_filtered, *pc_filtered, pose_vec[i].t, pose_vec[i].q);
-    downsample_voxel(*pc_filtered, downsample_size);
+  // loadCameraConfig(image_file);
+    cv::Mat camera_matrix_ =
+      (cv::Mat_<double>(3, 3) << camera_matrix[0], 0.0, camera_matrix[2], 0.0, camera_matrix[4], camera_matrix[5], 0.0, 0.0, 1.0);
+    cv::Mat dist_coeffs_ =
+      (cv::Mat_<double>(1, 5) << dist_coeffs[0], dist_coeffs[1], dist_coeffs[2], dist_coeffs[3], dist_coeffs[4]);
+    cv::Mat extrinsics =
+      (cv::Mat_<double>(4, 4) << extrinsic[0], extrinsic[1], extrinsic[2], extrinsic[3], extrinsic[4], extrinsic[5], extrinsic[6], extrinsic[7], extrinsic[8], extrinsic[9], extrinsic[10], extrinsic[11], extrinsic[12], extrinsic[13], extrinsic[14], extrinsic[15]);
+    // Camera intrinsics and extrinsics
 
-    pcl::toROSMsg(*pc_filtered, cloudMsg);
-    cloudMsg.header.frame_id = "camera_init";
-    cloudMsg.header.stamp = cur_t;
-    pub_map.publish(cloudMsg);
+    // cv::Mat undistortedImage;
+    // // std::cout<<camera_matrix<<distortion_coeff<<std::endl;
+    // cv::undistort(image, undistortedImage, camera_matrix_, dist_coeffs_);
+    // undistortedImage.copyTo(image);
 
-    geometry_msgs::Pose apose;
-    apose.orientation.w = pose_vec[i].q.w();
-    apose.orientation.x = pose_vec[i].q.x();
-    apose.orientation.y = pose_vec[i].q.y();
-    apose.orientation.z = pose_vec[i].q.z();
-    apose.position.x = pose_vec[i].t(0);
-    apose.position.y = pose_vec[i].t(1);
-    apose.position.z = pose_vec[i].t(2);
-    parray.poses.push_back(apose);
-    pub_pose.publish(parray);
+    //cv::Mat extrinsics = (cv::Mat_<double>(4, 4) <<
+    //    0.0240456,-0.999707,-0.00270886,0.134656,
+    //    0.0128452,0.00301838,-0.999913,-0.212597,
+    //    0.999628,0.0240088,0.0129141,-0.0985589,
+    //    0,0,0,1);
 
-    // static tf::TransformBroadcaster br;
-    // tf::Transform transform;
-    // transform.setOrigin(tf::Vector3(pose_vec[i].t(0), pose_vec[i].t(1), pose_vec[i].t(2)));
-    // tf::Quaternion q(pose_vec[i].q.x(), pose_vec[i].q.y(), pose_vec[i].q.z(), pose_vec[i].q.w());
-    // transform.setRotation(q);
-    // br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "camera_init", "turtle_name"));
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr colored_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    for (const auto& point : cloud->points) {
+        cv::Mat point3D = (cv::Mat_<double>(4, 1) << point.x, point.y, point.z, 1);
+        cv::Mat pointCam = extrinsics * point3D;
+        cv::Mat point2D = camera_matrix_ * pointCam.rowRange(0, 3);
+        point2D /= point2D.at<double>(2, 0);
 
-    // publish pose trajectory
-    visualization_msgs::Marker marker;
-    marker.header.frame_id = "camera_init";
-    marker.header.stamp = cur_t;
-    marker.ns = "basic_shapes";
-    marker.id = i;
-    marker.type = visualization_msgs::Marker::SPHERE;
-    marker.pose.position.x = pose_vec[i].t(0);
-    marker.pose.position.y = pose_vec[i].t(1);
-    marker.pose.position.z = pose_vec[i].t(2);
-    pose_vec[i].q.normalize();
-    marker.pose.orientation.x = pose_vec[i].q.x();
-    marker.pose.orientation.y = pose_vec[i].q.y();
-    marker.pose.orientation.z = pose_vec[i].q.x();
-    marker.pose.orientation.w = pose_vec[i].q.w();
-    marker.scale.x = marker_size; // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.y = marker_size;
-    marker.scale.z = marker_size;
-    marker.color.r = float(1-float(i)/pose_size);
-    marker.color.g = float(float(i)/pose_size);
-    marker.color.b = float(float(i)/pose_size);
-    marker.color.a = 1.0;
-    marker.lifetime = ros::Duration();
-    pub_trajectory.publish(marker);
+        int u = static_cast<int>(point2D.at<double>(0, 0));
+        int v = static_cast<int>(point2D.at<double>(1, 0));
 
-    // publish pose number
-    visualization_msgs::Marker marker_txt;
-    marker_txt.header.frame_id = "camera_init";
-    marker_txt.header.stamp = cur_t;
-    marker_txt.ns = "marker_txt";
-    marker_txt.id = i; // Any marker sent with the same namespace and id will overwrite the old one
-    marker_txt.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
-    ostringstream str;
-    str << i;
-    marker_txt.text = str.str();
-    marker.action = visualization_msgs::Marker::ADD;
-    marker_txt.action = visualization_msgs::Marker::ADD;
-    marker_txt.pose.position.x = pose_vec[i].t(0)+marker_size;
-    marker_txt.pose.position.y = pose_vec[i].t(1)+marker_size;
-    marker_txt.pose.position.z = pose_vec[i].t(2);
-    marker_txt.pose.orientation.x = pose_vec[i].q.x();
-    marker_txt.pose.orientation.y = pose_vec[i].q.y();
-    marker_txt.pose.orientation.z = pose_vec[i].q.x();
-    marker_txt.pose.orientation.w = 1.0;
-    marker_txt.scale.x = marker_size;
-    marker_txt.scale.y = marker_size;
-    marker_txt.scale.z = marker_size;
-    marker_txt.color.r = 1.0f;
-    marker_txt.color.g = 1.0f;
-    marker_txt.color.b = 1.0f;
-    marker_txt.color.a = 1.0;
-    marker_txt.lifetime = ros::Duration();
-    if(i%GAP == 0) markerArray.markers.push_back(marker_txt);
-    pub_pose_number.publish(markerArray);
+        if (u >= 0 && u < image.cols && v >= 0 && v < image.rows) {
+            cv::Vec3b color = image.at<cv::Vec3b>(v, u);
+            pcl::PointXYZRGB colored_point;
+            colored_point.x = point.x;
+            colored_point.y = point.y;
+            colored_point.z = point.z;
+            colored_point.r = color[2];
+            colored_point.g = color[1];
+            colored_point.b = color[0];
+            colored_cloud->points.push_back(colored_point);
+        }
+    }
 
-    ros::Duration(0.001).sleep();
-  }
+    colored_cloud->width = colored_cloud->points.size();
+    colored_cloud->height = 1;
+    colored_cloud->is_dense = false;
 
-  ros::Rate loop_rate(1);
-  while(ros::ok())
-  {
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
+	pcl::visualization::PCLVisualizer viewer;  //显示点云
+	viewer.addPointCloud<pcl::PointXYZRGB>(colored_cloud);
+	viewer.addCoordinateSystem(1.0);
+	while (!viewer.wasStopped())
+	{
+		viewer.spinOnce(100);
+	}
+
+    return 0;
 }
